728e62af6813625bfbc35841d484056c8c0f6a50
Justin <jsarraga@gmail.com>
2021-03-26 09:28:04 -0400

    CRUD functionality to Firebase. VueJS exercise

---
+++ src/components/showBlogs.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="show-blogs">
+      <h1>All Blog Articles</h1>
+      <input type="text" v-model="search" placeholder="search blogs" />
+      <div v-for="blog in filteredBlogs" :key="blog.id" class="single-blog">
+          <router-link v-bind:to="'blog/' + blog.id"><h2>{{blog.title | to-uppercase}}</h2></router-link>
+          <article>{{blog.content | snippet}}</article>
+      </div>
+  </div>
+</template>
+
+<script>
+import searchMixin from '../mixins/searchMixin';
+
+export default {
+
+  data() {
+    return {
+        blogs: [],
+        search: ""
+    }
+  },
+  methods: {
+    
+  },
+  created() {
+      this.$http.get('https://vueblog-ea590-default-rtdb.firebaseio.com/posts.json').then(function(data){
+          return data.json()
+      }).then(function(data){
+          var blogsArray = [];
+          for (let key in data){
+              data[key].id = key
+              blogsArray.push(data[key])
+          }
+          this.blogs = blogsArray
+      })
+  },
+  computed: {
+      
+  },
+  filters: {
+      toUppercase(value){
+          return value.toUpperCase();
+      },
+      snippet(value){
+          return value.slice(0,100) + '...'
+      }
+  },
+  directives: {
+      'rainbow': {
+          bind(el){
+              el.style.color = "#" + Math.random().toString().slice(2,8);
+          }
+      }
+        
+    },
+    mixins: [
+        searchMixin
+    ]
+}
+</script>
+
+<style>
+#show-blogs{
+    max-width: 800px;
+    margin: 0px auto;
+}
+.single-blog{
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eee;
+}
+</style>
